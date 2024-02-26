6ed4a8929f6ed36eff7bfd2f61cf6c7823a223b0
Mateja Adamovic <adamovicmateja@outlook.com>
2024-02-27 00:06:33 +0100

    SecondTry

--- Text analysis.py
+++ Text analysis.py
@@ -1,12 +1,17 @@
 def funcAnalyze(text):
     words=text.split()
     dictWords = {}
+    wordsNum = len(words)
+    avgLen = sum(len(word) for word in words)/wordsNum
     for word in words:
         dictWords[word] = dictWords.get(word,0) + 1
     mostCommon = sorted(dictWords.items(), key=lambda x: x[1], reverse=True)[:5]
     print("Most common words in the text:")
     for word, wordCount in mostCommon:
         print(f"{word}: {wordCount}")
+    print("Average word lenght is: " + avgLen)
+    print("Number of words is: " + wordsNum)
+    
 
 
 print("Enter the name of the text file to analyze: ")
